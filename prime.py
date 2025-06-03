729728c43496ce559ea1cc4b571ca8dd7985d34c
Harinianimela <harinianimela@gmail.com>
2025-06-03 11:02:28 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,18 @@
+n=2
+count=0
+for i in range(1,n+1):
+    if n%i==0:
+        count+=1
+print("prime" if count==2 else "not prime")
+
+
+
+n=2
+count=0
+if n==2:
+    print("prime")
+for i in range(2,n):
+    if n%i==0:
+        print("not prime")
+    else:
+        ("prime")
